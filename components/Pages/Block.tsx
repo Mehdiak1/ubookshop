83232f45096421caf447395e75f56e76a0c54e9e
Mehdiak1 <$mehdixwordpress@gmail.com>
2024-09-01 10:12:43 +0330

    Update

--- components/Pages/Block.tsx
+++ components/Pages/Block.tsx
@@ -10,13 +10,13 @@ export const Block = props => {
 
     return <c-c style={{
         width: 150, flex: 1, minWidth: 150,
-        position: "relative"
+        position: "relative", backgroundColor:"white",borderRadius:10,margin:"10px"
     }}>
 
         <img
             className={global.styles.hover}
             src={props.book.imageLink}
-            style={{ width: 150, height: 200, objectFit: "fill",  minWidth: 150 }}
+            style={{ width: "100%", height: 200, objectFit: "fill",  minWidth: 150, borderTopLeftRadius:10,borderTopRightRadius:10 }}
             onClick={() => {
                 props.state.form = "bookspecs"
                 props.state.book = props.book
@@ -27,13 +27,14 @@ export const Block = props => {
         <f-cc style={{width:"100%",padding:"5px 0"}}>
             <f-15>hiiiiiiiiii</f-15>
         </f-cc>
+        <hr style={{width:"80%" , opacity:0.2 , margin:"0 2px"}}/>
 
         <f-csb style={{width:"100%" , padding:"5px 0"}}>
             
-            <img src="https://irmapserver.ir/qepal/cart.svg" style={{width:30,height:30,objectFit:"contain"}}/>
+            <img src="https://irmapserver.ir/qepal/cart.svg" style={{width:30,height:30,objectFit:"contain", margin:"0 10px"}}/>
 
             
-            <c-x style={{direction:"ltr" , margin:"0 5px"}}>
+            <c-x style={{direction:"ltr" , margin:"0 10px"}}>
                 <f-12><del>۱۰۰۰۰ تومان</del></f-12>
                 <f-15>۱۵۰۰۰ تومان</f-15>
             </c-x>
